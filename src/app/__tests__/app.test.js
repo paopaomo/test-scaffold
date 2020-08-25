5bedcb39687335dad4c49f0324e3d210a1b29b41
shzeng <shzeng@thoughtworks.com>
2020-08-25 09:29:49 +0800

    setup test scaffold

---
+++ src/app/__tests__/app.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import App from '../app';
+
+describe('App', () => {
+  test('should render App component', () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('app').textContent).toBe('Hello World');
+  })
+});
